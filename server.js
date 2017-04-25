612066dd0109db68ea8951262763d54c516c0dd1
Roshni Kutty <roshni.ajkutty@gmail.com>
2017-04-25 19:39:09 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const { BlogPosts } = require('./models');
+
+const jsonParser = bodyParser.json();
+const app = express();
+
+app.use(morgan('common'));
+
+BlogPosts.create("My Ramblings1", "Here I go - bar", "Roshni", 4 - 25 - 2017);
+BlogPosts.create("My Ramblings2", "Here I go - foo", "Roshni", 1 - 2 - 2017);
+BlogPosts.create("My Ramblings3", "Here I go - bizz", "Roshni");
+
+app.get('/blogs', (req, res) => {
+    res.json(BlogPosts.get());
+});
+
+app.post('/blogs', jsonParser, (req, res) => {
+    const requiredFields = ['title', 'content', 'author'];
+    for (let i = 0; i < requiredFields.length; i++) {
+        const field = requiredFields[i];
+        if (!(field in req.body)) {
+            const message = `Missing \`${field}\` in request body`
+            console.error(message);
+            return res.status(400).send(message);
+        }
+    }
+
+    const blog = BlogPosts.create(req.body.title, req.body.content, req.body.author, req.body.publishDate);
+    res.status(201).json(blog);    
+});
+
+app.delete('/blogs/:id', (req, res) => {
+    BlogPosts.delete(req.params.id);
+    console.log(`Deleted blog with id \`${req.params.ID}\``);
+    res.status(204).end();
+});
+
+app.put('/blogs/:id',jsonParser, (req, res) => {
+    const updatedBlog = BlogPosts.update({
+        id:req.params.id,
+        title: req.body.title,
+        content: req.body.content,
+        author: req.body.author,
+        publishDate: req.body.publishDate
+    });
+    res.status(204).json(updatedBlog);
+});
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
