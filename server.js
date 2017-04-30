1b41091855316e5845035cb17fdf075f3eb80564
Roshni Kutty <roshni.ajkutty@gmail.com>
2017-04-30 08:45:35 -0400

    Added test-server and travis yml files, updated server.js to include runServer and closeServer

--- server.js
+++ server.js
@@ -49,6 +49,39 @@ app.put('/blogs/:id',jsonParser, (req, res) => {
     res.status(204).json(updatedBlog);
 });
 
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
-});
+// app.listen(process.env.PORT || 8080, () => {
+//   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+// });
+
+let server;
+
+function runServer(){
+    const port = process.env.PORT || 8080;
+    return new Promise((resolve, reject) => { 
+        server = app.listen(port, () => {
+            console.log(`Your app is listening at ${port}`);
+            resolve(server);
+            }).on('error', err =>{
+                reject(err)
+        })
+    });
+}
+
+function closeServer(){
+    return new Promise((resolve, reject) => {
+        console.log('Closing server');
+        server.close(err => {
+            if(err) {
+                reject(err);
+                return;
+            }
+            resolve();
+        });
+    });
+}
+
+if(require.main === module) {
+    runServer().catch(err => console.log(err));
+}
+
+module.exports = {app, runServer, closeServer};
---
+++ test/test-server.js
@@ -0,0 +1,89 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+const {app, runServer, closeServer} = require('../server');
+
+describe('Blogs', function(){
+
+    before(function(){
+        return runServer();
+    });
+
+    after(function(){
+        return closeServer();
+    });
+
+    it('should list items on GET', function(){
+        return chai.request(app)
+            .get('/blogs')
+            .then(function(res){
+                res.should.have.status(200);
+                res.should.be.json;
+                res.body.should.be.a('array');
+                res.body.length.should.be.at.least(1);
+                
+                const expectedKeys = ['id', 'title', 'content', 'author'];
+                
+                res.body.forEach(function(item){
+                    item.should.be.a('object');
+                    item.should.include.keys(expectedKeys);
+                });
+            });
+    });
+
+    it('should create item on POST', function(){
+        const newItem = {
+            title: "My Test Ramblings",
+            content: "Testing POST operation",
+            author: "Roshni"
+        };
+        return chai.request(app)
+            .post('/blogs')
+            .send(newItem)
+            .then(function(res){
+                res.should.have.status(201);
+                res.should.be.json;
+                res.should.be.a('object');
+                res.body.should.include.keys('id', 'title', 'content', 'author');
+                res.body.id.should.not.be.null;
+                res.body.should.deep.equal(Object.assign(newItem, {id: res.body.id}));
+            });
+    });
+
+    it('should update item on PUT', function(){
+        const updateData = {
+            title: "My Updated Test Ramblings",
+            content: "Testing PUT operation",
+            author: "Roshni"
+        };
+        return chai.request(app)
+        .get('/blogs')
+        .then(function(res){
+            updateData.id = res.body[0].id;
+            return chai.request(app)
+                .put(`/blogs/${updateData.id}`)
+                .send(updateData);
+        })
+        .then(function(res){
+            res.should.be.json;
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            res.body.should.deep.equal(updateData);
+        });
+    });
+
+    it('should delete item on POST', function(){
+        return chai.request(app)
+        .get('/blog')
+        .then(function(res){
+            return chai.request(app)
+                .delete(`/blog/${res.body[0].id}`);
+        })
+        .then(function(res){
+            res.should.have.status(204);
+        });
+    });
+});
